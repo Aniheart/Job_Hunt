a7444208b291cf02b83f00c597c112378a7d9f7a
Aniket <aniketk65310@gmail.com>
2026-09-05 11:23:00 +0530

    backend APIs checked

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,173 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body;
+    if (!fullname || !email || !phoneNumber || !password || !role) {
+      return res.status(404).json({
+        message: "Something is missing",
+        success: false,
+      });
+    }
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: " User already exist with this email",
+        success: false,
+      });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+    });
+    return res.status(201).json({
+      message: "Account created successfully.",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//login controller
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+    if (!email || !password || !role) {
+      // check if parameters are empty then throw a 400 error
+      return res.status(400).json({
+        message: "Something is missing",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email }); // check is the entered email is already exists (true)
+    if (!user) {
+      // if user not find again throw an error
+      return res.status(400).json({
+        message: "Incorrect email or password.",
+        success: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Incorect email or password",
+        success: false,
+      });
+    }
+
+    if (role !== user.role) {
+      return res.status(400).json({
+        message: "Account doesn't exist with current role.",
+        success: false,
+      });
+    }
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    const userData = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome back ${user.fullname}`,
+        user:userData,
+        success: true,
+      });
+  } catch (error) {
+    console.log(error);
+    
+  }
+};
+
+//LOGOUT CONTROLLER
+
+export const logout = async (req, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "logged out successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, bio, skills } = req.body;
+    const file = req.file;
+
+    //cloudinary comes here
+    let skillsArray;
+    if(skills) {
+    skillsArray = skills.split(",");
+    }
+    
+    const userId = req.id; // middleware authentication
+    let user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({
+        message: "user not found",
+        success: false,
+      });
+    }
+    //updating data
+    if(fullname) user.fullname = fullname;
+    if(email) user.email = email;
+    if(phoneNumber) user.phoneNumber = phoneNumber;
+    if(bio) user.profile.bio = bio;
+    if(skills) user.profile.skills = skillsArray;
+
+    //resume comes here later
+
+    await user.save();
+
+     const userData = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res.status(200).json({
+        message:"profile updated successfully.",
+        user:userData,
+        success:true
+    })
+
+  } catch (error) {
+     console.log(error);
+
+  return res.status(500).json({
+    message: "Internal server error",
+    success: false,
+  });
+  }
+};
---
+++ backend/routes/user.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import isAuthenticated from "../middlewares/isAuthenticated.js";
+
+import {
+    register,
+    login,
+    updateProfile
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.route("/register").post(register);
+
+router.route("/login").post(login);
+
+router.route("/profile/update").post(isAuthenticated, updateProfile);
+
+export default router;
